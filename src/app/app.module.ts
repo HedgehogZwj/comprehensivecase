2383ee178d4778d00b456d1c775a418091d59042
Zwj <HedgehogZwj@163.com>
2020-05-14 20:15:31 +0800

    组件创建完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IntroduceComponent } from './introduce/introduce.component';
+import { HeadComponent } from './head/head.component';
+import { UpdateLogComponent } from './update-log/update-log.component';
+import { ContactComponent } from './contact/contact.component';
+import { ManagementComponent } from './management/management.component';
+import { LoginComponent } from './login/login.component';
+import { ScoreComponent } from './score/score.component';
+import { ExitComponent } from './exit/exit.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IntroduceComponent,
+    HeadComponent,
+    UpdateLogComponent,
+    ContactComponent,
+    ManagementComponent,
+    LoginComponent,
+    ScoreComponent,
+    ExitComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
